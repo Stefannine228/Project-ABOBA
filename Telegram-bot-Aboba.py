c72d56c1a6386dc1ad7005a0a9efca0c90c06110
Stefannine228 <154265867+Stefannine228@users.noreply.github.com>
2025-03-02 21:07:27 +0200

    Update Telegram-bot-Aboba.py

--- Telegram-bot-Aboba.py
+++ Telegram-bot-Aboba.py
@@ -1,8 +1,14 @@
+import os
 import telebot
 from telebot import types
-import time
+from flask import Flask
 
-bot = telebot.TeleBot("7667965160:AAFVbRt8GeYhusJZx93u1953VfOabIRXR3o")
+bot = telebot.TeleBot("ТВОЙ_ТОКЕН")
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Бот працює!"
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -12,9 +18,16 @@ def start_message(message):
 def main(message):
     bot.send_message(message.chat.id, "/start - запуск бота \n/help - усі команди")
 
-while True:
-    try:
-        bot.polling(none_stop=True, timeout=60)
-    except Exception as e:
-        print(f"Сталася помилка: {e}")
-        time.sleep(5)  # Пауза перед перезапуском
+def start_bot():
+    while True:
+        try:
+            bot.polling(none_stop=True, timeout=60)
+        except Exception as e:
+            print(f"Помилка: {e}")
+
+if __name__ == '__main__':
+    from threading import Thread
+    Thread(target=start_bot).start()
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
