5a31d137fd527739a5e660c4e4f024f643cbc61a
Stefannine228 <154265867+Stefannine228@users.noreply.github.com>
2025-03-02 19:51:14 +0200

    Update Telegram-bot-Aboba.py

--- Telegram-bot-Aboba.py
+++ Telegram-bot-Aboba.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("7667965160:AAFVbRt8GeYhusJZx93u1953VfOabIRXR3o")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Салаам Алейкум, і я продовжу терор на новому рівні. МУХАХАХАХА")
+    bot.send_message(message.chat.id, "Абоба")
 @bot.message_handler(commands=['help'])
 def main(message):
     bot.send_message(message.chat.id, "/start - запуск бота \n /help - усі команди")
